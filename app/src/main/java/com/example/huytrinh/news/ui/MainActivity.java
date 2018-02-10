d2ea5ccfbcdde2c77778bea6ef5be8dc3cde74ef
HuyTrinh <dinhhuyams@gmail.com>
2018-02-10 12:59:00 +0200

    add icon to splash activity

--- app/src/main/java/com/example/huytrinh/news/ui/MainActivity.java
+++ app/src/main/java/com/example/huytrinh/news/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.huytrinh.news;
+package com.example.huytrinh.news.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.huytrinh.news.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/huytrinh/news/ui/SplashActivity.java
+++ app/src/main/java/com/example/huytrinh/news/ui/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.example.huytrinh.news.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.huytrinh.news.R;
+import com.example.huytrinh.news.util.Constants;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        startActivity(MainActivity.class);
+    }
+
+    private void startActivity(final Class<? extends Activity> token) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, token);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }, Constants.SPLASH_DELAY);
     }
 }
